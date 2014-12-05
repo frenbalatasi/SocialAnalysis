aadeab8f154d130ae741ba979190af939590bac3
PC-Lab <PC-Lab@uni-klu>
2014-12-05 19:14:37 +0100

    Finally EditText view behaves as I wanted. It takes the full focus on it when it's clicked and when it's not selected, it loses its focus.

--- src/at/ac/uniklu/crosmos/socialanalysis/MainActivity.java
+++ src/at/ac/uniklu/crosmos/socialanalysis/MainActivity.java
@@ -6,6 +6,7 @@ import java.util.List;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -62,12 +63,13 @@ public class MainActivity extends Activity {
 		nAdapter.notifyDataSetChanged();
 		listView.setAdapter(nAdapter);
 		
+		editTextBottom.setCursorVisible(false);
+		editTextBottom.setFocusable(false);
+	    editTextBottom.setFocusableInTouchMode(false);
+		
 		spAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, noteType);
 	    spinner.setAdapter(spAdapter);
     	
-	    editTextBottom.setFocusable(false);
-	    editTextBottom.setFocusableInTouchMode(false);
-	    
     	toggleListeners();
 
 	}
@@ -111,18 +113,20 @@ public class MainActivity extends Activity {
 			}
 		});
 		
-//		// Show the keyboard when EditText at the bottom has a focus on it
-//		editTextBottom.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//		    @Override
-//		    public void onFocusChange(View v, boolean hasFocus) {
-//		        if (hasFocus) {
-//		        	showSoftKeyboard(editTextBottom);
-//		        }
-//		        else {
-//		        	hideSoftKeyboard(editTextBottom);
-//		        }
-//		    }
-//		});
+		// Show the keyboard when EditText at the bottom has a focus on it
+		editTextBottom.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+		    @Override
+		    public void onFocusChange(View v, boolean hasFocus) {
+		        if (hasFocus) {
+		        	showAsToast("I got the attention NOW! :)");
+		        	showSoftKeyboard(editTextBottom);
+		        }
+		        else {
+		        	showAsToast("I lost the attention :(");
+		        	hideSoftKeyboard(editTextBottom);
+		        }
+		    }
+		});
 		
 		// ClickListener for Send Button
 		buttonSend.setOnClickListener(new View.OnClickListener() {
@@ -133,25 +137,29 @@ public class MainActivity extends Activity {
             		newTxtNote.setText(editTextBottom.getText().toString());
             		notesList.add(newTxtNote);
             		
-            		editTextBottom.setCursorVisible(false);
-            		hideSoftKeyboard(editTextBottom);
+            		editTextBottom.setText("");
+                	editTextBottom.setCursorVisible(false);
+                	editTextBottom.setFocusable(false);
+                	editTextBottom.setFocusableInTouchMode(false);
             	}
             	
             	else if(positionOfSpinner == 1) {
             		AudioNotes newAudioNote = new AudioNotes();
-            		nAdapter.addData(newAudioNote);
+            		notesList.add(newAudioNote);
             	}
             	
             	else if(positionOfSpinner == 2) {
             		VideoNotes newVideoNote = new VideoNotes();
-            		nAdapter.addData(newVideoNote);
+            		notesList.add(newVideoNote);
             	}
+            
+            	listView.setFocusable(false);
+            	listView.setFocusableInTouchMode(false);
             	
             	nAdapter.notifyDataSetChanged();
             	addNoteToServer();
-            	
-            	editTextBottom.setText("");
             }
+            
         });
 		
 		// When EditText at the bottom has been touched, give the focus on it.
@@ -170,21 +178,22 @@ public class MainActivity extends Activity {
 			  @Override
 		      public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 				  	positionOfSpinner = arg2;
-		            
+				  	
 		            if(positionOfSpinner == 0) {
-		            	editTextBottom.setVisibility(View.VISIBLE);
-		            	imageView.setVisibility(View.INVISIBLE);
-		            	
-		            	editTextBottom.setCursorVisible(true);
+	            		editTextBottom.setCursorVisible(true);
 		            	editTextBottom.setFocusable(true);
 		            	editTextBottom.setFocusableInTouchMode(true);
-		            	showSoftKeyboard(editTextBottom);
+		            	
+		            	editTextBottom.setVisibility(View.VISIBLE);
+		            	imageView.setVisibility(View.INVISIBLE);
 		            	
 		            	showAsToast("Type mode: ON");
 		            }
 		            	
 		            if(positionOfSpinner == 1 || positionOfSpinner == 2) {
-		            	hideSoftKeyboard(editTextBottom);
+		            	editTextBottom.setCursorVisible(false);
+		            	editTextBottom.setFocusable(false);
+		            	editTextBottom.setFocusableInTouchMode(false);
 		            	
 		            	editTextBottom.setVisibility(View.INVISIBLE);
 		            	imageView.setVisibility(View.VISIBLE);
@@ -212,11 +221,11 @@ public class MainActivity extends Activity {
 	/** Show the soft keyboard in the activity */
 	private void showSoftKeyboard(EditText edtTxt) {
 		InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-		imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+		imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT,0);
 	}
 	
 	/** Show the related text as Toast to MainActivity */
 	private void showAsToast(String message) {
-		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
 	}
 }
